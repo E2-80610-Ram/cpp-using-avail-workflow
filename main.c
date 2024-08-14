f0d17a737277614bce537801481fb1431070ff8e
BAJGIRE RAM NAMDEV <144893867+E2-80610-Ram@users.noreply.github.com>
2024-08-14 10:39:38 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
+int calculation(int a,int b)
+{
+  int c =a+b;
+}
 int main()
 {
-  printf("github actions");
+  printf("github actions tutorial\n");
+  int a,b,c;
+  scanf("%d%d",&a,&b);
+  c= calculation(a,b);
+  FILE *file = fopen("result.txt", "w");
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    fprintf(file, "%d\n", c);
+    fclose(file);
+   printf("Result calculated and saved to result.txt\n");
   return 0;
 }
